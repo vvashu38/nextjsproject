4ceaf2a181fbb084e37009a6a1091b6e267fc01e
Subodh Vashistha <vashistha3812@gmail.com>
2024-10-26 13:18:49 +0530

    Project

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me Now here
+    
+      </Button>
+    </div>
+  )
+}
